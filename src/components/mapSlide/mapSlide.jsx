12421e17f6b90b689b0b8471c7a945f51838b031
Nemanja Mijailovic <nemanja.mijailovic2912@gmail.com>
2024-08-13 20:51:36 +0200

    fix

--- src/components/mapSlide/mapSlide.jsx
+++ src/components/mapSlide/mapSlide.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
 import styles from "./mapSlide.module.css";
 
-const MapSlide = ({ slideRef, title, text, image }) => {
+const MapSlide = ({ slideRef, title, text, image, element }) => {
+  const [stickyTop, setStickyTop] = useState(0);
+  useEffect(() => {
+    const getCalculation = () => {
+      return (
+        window.innerHeight - document.querySelector("#mapSlide").getBoundingClientRect().height
+      );
+    };
+
+    const handleSticky = () => {
+      setStickyTop(getCalculation());
+    };
+
+    window.addEventListener("resize", handleSticky);
+
+    setTimeout(() => {
+      handleSticky();
+    }, 500);
+
+    return () => window.removeEventListener("resize", handleSticky);
+  }, []);
   return (
-    <div ref={slideRef} className={styles.slide}>
-      <div className={styles.imageWrapper}>
-        <img src={image} alt="" />
-      </div>
-      <div className={styles.content}>
-        <div className={styles.title}>{title}</div>
-        <div className={styles.text}>{text}</div>
+    <div id="mapSlide" ref={slideRef} className={styles.slide} style={{ top: stickyTop }}>
+      <div className={styles.slideContent}>
+        <div className={styles.imageWrapper}>
+          <img src={image} alt="" />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.text}>{text}</div>
+        </div>
       </div>
     </div>
   );
